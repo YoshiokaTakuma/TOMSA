430043bbd9d047e2cb9b626d9f9ee787a3363809
jinji-pc <jinji-pc>
2016-08-23 10:01:21 +0900

    昨日（2016/08/22）の作業を踏まえて、行列を計算するまで。
    マトリクスの生成ロジックを改良したら、1次元は完成！

--- 1D_springs.py
+++ 1D_springs.py
@@ -4,14 +4,11 @@ import pandas as pd
 # データの読み込み
 node = pd.read_csv('node.csv')
 spring = pd.read_csv('spring.csv')
-
-print(spring)
-
-# 各節点にかかる力
-force = np.array(node.ix[:,'force'])
+print(spring, '\n')
 
 # 各ばねのばね定数
 k = spring.ix[:, 'constant']
+
 
 # 剛性マトリクス
 # 現状だと一次元かつ、バネが順番に並んでいないと剛性マトリクスを正しく生成できない
@@ -58,7 +55,7 @@ node['Eq_ID'] = eqlist
 node_s = node.sort_values(by='Eq_ID')
 # ソートしたデータフレームのindex番号を付け直す
 node_s = node_s.reset_index(drop=True)
-print(node_s)
+print(node_s, '\n')
 
 
 # マトリックスの列を、ソートしたPoint_ID順に新しい行列に入れていく
@@ -72,4 +69,20 @@ for i in list(range(node_s.ix[:, 0].size)):
     arr2 = np.vstack((arr2, arr[node_s.ix[i, 'Point_ID']-1, :]))
     
 print('変形後の全体剛性マトリクス')
-print(arr2) 
+print(arr2, '\n') 
+
+# マトリクスのうち、必要な部分を抽出
+arr3 = arr2[0:len(free), 0:len(free)]
+print('抽出後のマトリクス')
+print(arr3, '\n')
+
+# マトリクスに合わせて、力ベクトルを取得
+force = np.array(node_s.ix[0:len(free)-1,'force'])
+
+# 連立方程式を解く
+x = np.linalg.solve(arr3, force)
+
+for i in list(range(len(free),len(free) + len(fix))):
+    print('u', node_s.ix[i,'Point_ID'], '= 0.0')
+for i in list(range(force.size)):
+    print('u', node_s.ix[i,'Point_ID'], '=', x[i])
