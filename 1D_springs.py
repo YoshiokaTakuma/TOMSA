5df85aba689c7c3914f93fea5d8e98f2a58cd6bc
jinji-pc <jinji-pc>
2016-08-24 14:55:46 +0900

    誤字修正

--- 1D_springs.py
+++ 1D_springs.py
@@ -11,18 +11,18 @@ print('ばねの情報')
 print(spring, '\n')
 
 # よく使う定数と配列の定義
-martix_size = node.ix[:, 0].size
+matrix_size = node.ix[:, 0].size
 spring_number = spring.ix[:, 0].size
 k = spring.ix[:, 'constant']
 
 # 支持条件を読み込んで、方程式IDと節点の順番を結びつけて辞書に登録
 free = []
-for i in list(range(martix_size)):
+for i in list(range(matrix_size)):
     if node.ix[i,2] == 'free':
         free.append(node.ix[i,0])
 
 fix = []
-for i in list(range(martix_size)):
+for i in list(range(matrix_size)):
     if node.ix[i,2] == 'fix':
         fix.append(node.ix[i,0])
 
@@ -33,12 +33,12 @@ dic = dict(zip(point_order, eq_id))
 
 # 剛体マトリクスの生成
 # 節点数の大きさの０行列
-matrix = np.zeros((martix_size, martix_size))
+matrix = np.zeros((matrix_size, matrix_size))
 
 # バネ定数を方程式IDの該当箇所に加算
 for spring_id in list(range(spring_number)):
-    for point_id in list(range(martix_size)):
-        element_matrix = np.zeros((martix_size, martix_size))
+    for point_id in list(range(matrix_size)):
+        element_matrix = np.zeros((matrix_size, matrix_size))
         if spring.ix[spring_id, 'Point1'] == point_id+1:
             for i in list(range(1, 3)):
                 for j in list(range(1, 3)):
@@ -58,7 +58,7 @@ print(part_matrix, '\n')
 
 # 支持条件='free'の力ベクトルを抽出
 force = []
-for i in list(range(martix_size)):
+for i in list(range(matrix_size)):
     if node.ix[i, 'support'] == 'free':
         force.append(node.ix[i, 'force'])
 force = np.array(force)
@@ -70,7 +70,7 @@ x = np.linalg.solve(part_matrix, force)
 # 結果出力
 print('結果（変位）')
 count_fix = 0
-for i in list(range(martix_size)):
+for i in list(range(matrix_size)):
     if node.ix[i, 'support'] == 'fix':
         count_fix = count_fix + 1
         print('u', node.ix[i,'Point_ID'], '= 0.0')        
